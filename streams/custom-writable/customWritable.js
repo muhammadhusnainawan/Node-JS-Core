159732865d98dedc4b38029f466c0bfbfd394c80
muhammadhusnainawan <awan.husnain@gmail.com>
2024-03-03 17:53:52 +0500

    custom writable stream

--- streams/custom-writable/customWritable.js
+++ streams/custom-writable/customWritable.js
@@ -1,4 +1,4 @@
-const { Writable } = require("streams");
+const { Writable } = require("node:stream");
 const fs = require("node:fs");
 
 class FileWriteStream extends Writable {
@@ -32,17 +32,75 @@ class FileWriteStream extends Writable {
     this.chunkSize += chunk.length;
     if (this.chunkSize > this.writableHighWaterMark) {
       fs.write(this.fd, Buffer.concat(this.chunks), (err) => {
-        if (err) {
-          return callback(err);
-        }
+        if (err) return callback(err);
         this.chunks = [];
         this.chunkSize = 0;
         ++this.writesCount;
-        callback();
       });
     } else {
-        // when we are done we should call the callback
+      // when we are done we should call the callback
       callback();
     }
   }
+
+  _final(callback) {
+    fs.write(this.fd, Buffer.concat(this.chunks), (err) => {
+      if (err) return callback(err);
+      ++this.writesCount;
+      this.chunks = [];
+      callback();
+    });
+  }
+
+  _destroy(error, callback) {
+    console.log("Number of writes :", this.writesCount);
+    if (this.fd) {
+      fs.close(this.fd, (err) => {
+        callback(err || error);
+      });
+    } else {
+      callback(error);
+    }
+  }
 }
+
+(async () => {
+  console.time("writeMany");
+
+  const stream = new FileWriteStream({ fileName: "text.txt" });
+
+  let i = 0;
+
+  const numberOfWrites = 10000000;
+
+  const writeMany = () => {
+    while (i < numberOfWrites) {
+      const buff = Buffer.from(`${i}`, "utf-8");
+
+      // this is our last write
+      if (i === numberOfWrites - 1) {
+        return stream.end(buff);
+      }
+
+      // if stream.write returns false, stop the loop
+
+      if (!stream.write(buff)) break;
+
+      i++;
+    }
+  };
+
+  writeMany();
+
+  let d = 0;
+  // resume our loop once our stream's internal buffer is emptied
+  stream.on("drain", () => {
+    ++d;
+    writeMany();
+  });
+
+  stream.on("finish", () => {
+    console.log("Number of drains:", d);
+    console.timeEnd("writeMany");
+  });
+})();
